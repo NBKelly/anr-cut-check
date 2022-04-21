038344e8748a38478036f7b54cc2d471b5ef0b08
NB Kelly <N.B.Kelly@protonmail.com>
2022-04-21 15:33:12 +1200

    first pass 'is it safe to ID?'

--- com/nbkelly/outcomes/Outcomes.java
+++ com/nbkelly/outcomes/Outcomes.java
@@ -77,22 +77,89 @@ public class Outcomes extends Drafter {
 	    DEBUGF(1, "%s vs %s%n", entry.getKey(), entry.getValue());
 
 	DEBUG();
-	
-	calculate_outcomes(opps,
-			   scores,
-			   free,
-			   roundCount,
-			   cutSize);
 
-	
+	if(free.size() == 0)
+	    calculate_outcomes(opps, scores, free, roundCount, cutSize, false);
+	else {
+	    println(calculate_outcomes(opps, scores, free, roundCount, cutSize, false));
+	    println(calculate_outcomes(opps, scores, free, roundCount, cutSize, true));
+	}
+
+	safeToId(opps, scores, free, roundCount, cutSize);
 	
 	return DEBUG(1, t.split("Finished Processing"));
     }
 
-    private void calculate_outcomes(HashMap<String, TreeSet<String>> opponents,
+    private TreeSet<String> safeToId(HashMap<String, TreeSet<String>> opponents,
+				     HashMap<String, Integer> scores,
+				     HashMap<String, String> free,
+				     int num_rounds, int cut_size) {
+	TreeSet<String> free_players = new TreeSet<>();
+
+	for(var entry : free.entrySet()) {
+	    free_players.add(entry.getKey());
+	    free_players.add(entry.getValue());
+	}
+
+	TreeSet<String> safe_to_id = new TreeSet<String>();
+	for(var player : free_players) {
+	    if(safeToId(player, opponents, scores, free, num_rounds, cut_size))
+		safe_to_id.add(player);
+	}
+
+	return safe_to_id;
+    }
+
+    private boolean safeToId(String player, HashMap<String, TreeSet<String>> opponents,
+			     HashMap<String, Integer> scores, //clone
+			     HashMap<String, String> free, //clone
+			     int num_rounds, int cut_size) {
+	//remove the player from the set of free rounds, and update scores
+	
+	var new_free = (HashMap<String, String>)(free.clone());
+
+	var scenario = (HashMap<String, Integer>)(scores.clone());		
+	
+
+	//pick the first game free
+	for(var entry : new_free.entrySet())
+	    if(entry.getKey().equals(player) || entry.getValue().equals(player)) {
+		new_free.remove(entry.getKey());
+		//update scores
+		scenario.put(entry.getKey(), scenario.get(entry.getKey()) + 3); //split city
+		scenario.put(entry.getValue(), scenario.get(entry.getValue()) + 3); //split city
+		break;
+	    }
+
+	var outcomes = new HashMap<String, Integer>();
+
+	//all scenarios are open (nobody is playing perfectly)
+	calculate_recursive(opponents, scenario, new_free, num_rounds, cut_size,
+			    outcomes, false);
+
+	//find ourselves in outcomes if possible
+	if(!outcomes.containsKey(player))
+	    return false;
+
+	var outcome = outcomes.get(player);
+
+	//see if it's 100%
+	var total = 0;
+	for(var entry : outcomes.entrySet())
+	    total += entry.getValue();
+
+	DEBUGF("ID odds for %s: %6.3f%n", player, (outcome / (double)total) *(100*cut_size));
+	
+	return outcome * cut_size == total;
+    }
+
+    /* todo: needs to ID */
+    
+    private String calculate_outcomes(HashMap<String, TreeSet<String>> opponents,
 				    HashMap<String, Integer> scores,
 				    HashMap<String, String> free,
-				    int num_rounds, int cut_size) {
+				    int num_rounds, int cut_size,
+				    boolean twofourone) {
 	if(free.size() == 0) {
 	    println(" FIXED RESULT");
 	    println("==============");
@@ -124,60 +191,37 @@ public class Outcomes extends Drafter {
 		    break;
 		else
 		    printf("%2dst seed: %s%n", index++, standing.name);
+
+	    return null;
 	}
 	else {
 	    //we become a first year cs student: do all posibilities,
 	    //how do we do this, and what results do we get?
 	    //we want a set of name -> number of times made the cut
-	    {
-		var twofourone = true;
-		var outcomes = new HashMap<String, Integer>();
+	    StringBuilder res = new StringBuilder();
 
-		calculate_recursive(opponents, scores, free, num_rounds, cut_size,
-				    outcomes, twofourone);
-		
-		int total = 0;
-		for(var entry : outcomes.entrySet())
-		    if(entry.getKey() != "(Bye)")
-			total += entry.getValue();
-		
-		ArrayList<Odd> odds = new ArrayList<Odd>();
-		
-		for(var entry : outcomes.entrySet())
-		    odds.add(new Odd(entry.getKey(), entry.getValue(), total, cut_size));
-		
-		Collections.sort(odds);
-		
-		printf("ODDS FOR TOP %d CUT (241's enforced):%n", cut_size);
-		for(var odd : odds)
-		    println(odd);
-	    }
+	    var outcomes = new HashMap<String, Integer>();
 
-	    println();
+	    calculate_recursive(opponents, scores, free, num_rounds, cut_size,
+				outcomes, twofourone);
+		
+	    int total = 0;
+	    for(var entry : outcomes.entrySet())
+		if(entry.getKey() != "(Bye)")
+		    total += entry.getValue();
+		
+	    ArrayList<Odd> odds = new ArrayList<Odd>();
+		
+	    for(var entry : outcomes.entrySet())
+		odds.add(new Odd(entry.getKey(), entry.getValue(), total, cut_size));
+		
+	    Collections.sort(odds);
+		
+	    res.append(String.format("ODDS FOR TOP %d CUT (241's enforced):%n", cut_size));
+	    for(var odd : odds)
+		res.append(odd + "\n");
 
-	    {
-		var twofourone = false;
-		var outcomes = new HashMap<String, Integer>();
-
-		calculate_recursive(opponents, scores, free, num_rounds, cut_size,
-				    outcomes, twofourone);
-		
-		int total = 0;
-		for(var entry : outcomes.entrySet())
-		    if(entry.getKey() != "(Bye)")
-			total += entry.getValue();
-		
-		ArrayList<Odd> odds = new ArrayList<Odd>();
-		
-		for(var entry : outcomes.entrySet())
-		    odds.add(new Odd(entry.getKey(), entry.getValue(), total, cut_size));
-		
-		Collections.sort(odds);
-		
-		printf("ODDS FOR TOP %d CUT (all scenarios):%n", cut_size);
-		for(var odd : odds)
-		    println(odd);
-	    }
+	    return res.toString();
 	}
     }
 
@@ -257,11 +301,11 @@ public class Outcomes extends Drafter {
 				outcomes, twofourone);
 
 	    if(!twofourone) {
-		var s2 = (HashMap<String, Integer>)(scores.clone());
-		calculate_recursive(opponents, s2, new_free, num_rounds, cut_size,
-				    outcomes, twofourone);
+		var s2 = (HashMap<String, Integer>)(scores.clone());		
 		s2.put(left_player, s2.get(left_player) + 3); //split city
 		s2.put(right_player, s2.get(right_player) + 3); //split city
+		calculate_recursive(opponents, s2, new_free, num_rounds, cut_size,
+				    outcomes, twofourone);
 	    }
 	}
     }
