29b92351b47885805e275bc75cbf8bd058b4a588
NB Kelly <N.B.Kelly@protonmail.com>
2022-06-26 19:41:26 +1200

    only pull swiss

--- Pairings.py
+++ Pairings.py
@@ -11,15 +11,22 @@ TOURNEY= URL + TID + EXT
 session = HTMLSession()
 page = session.get(TOURNEY)
 soup = BeautifulSoup(page.content, "html.parser")
-rows = soup.find_all('div', attrs={'class':'round_pairing'})
+accordions = soup.find_all('div', attrs={'class':'accordion'})
 
-for row in rows:
-    left = row.find('div', attrs={'class':'left_player_name'})
-    right = row.find('div', attrs={'class':'right_player_name'})
-    scores = row.find('div', attrs={'class':'centre_score'}).text
+def process_rows(rows):
+    for row in rows:
+        left = row.find('div', attrs={'class':'left_player_name'})
+        right = row.find('div', attrs={'class':'right_player_name'})
+        scores = row.find('div', attrs={'class':'centre_score'}).text
 
-    print(left.contents[0].strip())
-    print(scores.split("-")[0].strip())
-    
-    print(right.contents[0].strip())
-    print(scores.split("-")[1].strip())
+        print(left.contents[0].strip())
+        print(scores.split("-")[0].strip())
+
+        print(right.contents[0].strip())
+        print(scores.split("-")[1].strip())
+
+for block in accordions:
+    # assert that this is swiss
+    acc_type = block.find('h4')
+    if acc_type.text.strip() == "Swiss":
+        process_rows(block.find_all('div', attrs={'class':'round_pairing'}))
